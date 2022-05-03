fd2999c364bca5f625cb77422fbef9d6d632ebf5
Samuli Tapio <samuli996@gmail.com>
2022-05-03 16:16:42 +0300

    Added the panels and their locations

--- elderScribbles/Main.java
+++ elderScribbles/Main.java
@@ -6,5 +6,11 @@ public class Main {
 
 	public static void main(String [] args) {
 		
+		MainFrame mainframe = new MainFrame();
+		mainframe.addPanel(new SidePanel().getPanel(), "WEST");
+		mainframe.addPanel(new CenterPanel().getPanel(), "CENTER");
+		mainframe.addPanel(new TopPanel().getPanel(), "NORTH");
+		mainframe.setVisible();
+
 	}
 }
