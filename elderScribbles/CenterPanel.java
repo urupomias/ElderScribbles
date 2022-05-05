056ea94781ac5aade3a22146c89104e90b48348b
urupomias <urupomias@hotmail.com>
2022-05-05 17:25:05 +0300

    doublesure

--- elderScribbles/CenterPanel.java
+++ elderScribbles/CenterPanel.java
@@ -2,17 +2,25 @@ package elderScribbles;
 import java.awt.Color;
 import java.awt.Dimension;
 import javax.swing.JPanel;
+import javax.swing.JTextPane;
+import java.awt.event.*;
+import javax.swing.*;
 
 public class CenterPanel {
     JPanel panel;
+    JTextField textfield;
+    Note note;
 
     public CenterPanel(){
         panel = new JPanel();
         panel.setBackground(Color.white);
         //panel.setPreferredSize(new Dimension(250,250));
+        this.textfield = new JTextField();
+        //textfield.addActionListener(this);
     }
 
     public JPanel getPanel(){
         return panel;
     }
 }
+
