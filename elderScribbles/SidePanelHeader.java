d00aa8106fee4df16b39c319899ea91e4367c2bb
Samuli Tapio <samuli996@gmail.com>
2022-05-12 04:43:16 +0300

    New class

---
+++ elderScribbles/SidePanelHeader.java
@@ -0,0 +1,36 @@
+package elderScribbles;
+
+import javax.swing.JPanel;
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.util.ArrayList;
+import java.util.List;
+import java.awt.FlowLayout;
+
+import javax.swing.JButton;
+import javax.swing.JLabel;
+
+
+public class SidePanelHeader{
+    
+    private String text;
+    private int indentation1 = -5;
+    private int indentation2 = -5;
+    private int indentation3 = -5;
+
+    public SidePanelHeader(String text, int indentation1){
+        this.text = text;
+        this.indentation1 = indentation1;
+
+    }
+
+    public String getText(){
+        return text;
+    }
+    public int getIndentation(){
+        return indentation1;
+    }
+
+}
